b0cbecbeaed12b7852ff19e72896f6d55d98b8b0
intbjw <a1051393709@qq.com>
2020-01-18 17:22:50 +0800

    add

--- 1024.py
+++ 1024.py
@@ -1,20 +1,24 @@
 import re
-
 s = input()
 m = r'([+-])(\d+.\d+)E([+-])0*(\d+)'
 
 
 def replace(m):
     N = eval(m.group(1) + m.group(2))
-    print(N)
     M = eval(m.group(4))
-    print(M)
+    flag = 1
     if m.group(3) == '+':
+        flag = -1
         res = N * (10 ** M)
     else:
         res = N / (10 ** M)
-
+    if m.group(3) == '+' and M >= (s.find('E') - s.find('.')):
+        w = 0
+    else:
+        w = s.find('E') - s.find('.') + M * flag - 1
+    res = format(res, '.'+str(w)+'f')
     return str(res)
 
 
 print(re.sub(m, replace, s))
+
